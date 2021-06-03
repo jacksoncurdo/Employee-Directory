7076b2197b552201e93398cadbeeb1a9bfaf2b53
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-02 19:41:14 -0600

    created components and wrote the code for app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import API from "./utils/API";
+import AllEmployees from "./Components/AllEmployees";
+import SearchForm from "./Components/SearchForm";
+import EmployeeSort from "./Components/EmployeeSort";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Setting the state to filtered results 
+class App extends Component {
+  constructor() {
+    super(); 
+    this.state = {
+      search: "",
+      users: [],
+      filterResults: []
+    }; 
+  }
+  
+  // Calling the "Random User API when the page loads then updating the state so that the user see the results of the API call"
+  componentDidMount() {
+    API.search().then((results) => {
+      this.setState({ users: results.data.results });
+    });
+
+  }
+  handleInputChange = (event) => {
+    this.setState({search:event.target.value})
+  };
+
+  // This is using the state to filer the results and return first name and last name of a searched employee
+  render() {
+    let filterResults = this.state.users.filter((each) => {
+      return (
+        each.name.first
+          .toLowerCase()
+          .includes(this.state.search.toLowerCase()) ||
+        each.name.last
+          .toLowerCase()
+          .includes(this.state.search.toLowerCase()));
+    });
+
+    // This is returning the html with a search form to search employees by name or email using sort to filter the results
+    return (
+      <div className="App">
+        <div className="background">
+        <div className="header">
+          <h1 className="display-4">Employee Directory</h1>
+          <p> Search for an employee by entering their name or email below.</p>
+          
+          <SearchForm 
+        handleInputChange={this.handleInputChange}
+        name={this.search}
+         />
+        </div>
+        {<EmployeeSort emailSort={this.emailSort}/>}
+        <AllEmployees users={filterResults} /> 
+      </div>
+      </div>
+     
+      
+    );
+  }
 }
 
 export default App;
