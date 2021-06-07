b4e5d18e6f03b0bc8ff027bd290f52a03cf5561a
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-06 19:17:53 -0600

    created employee list

---
+++ my-app/src/Components/EmployeeList/index.js
@@ -0,0 +1,18 @@
+const EmployeeList = (props) => {
+    return (
+    
+        <tr>
+          <td>
+        <img alt="user" src={props.user.picture.thumbnail}></img>
+        </td>
+        <td>{props.user.name.first}</td>
+        <td>{props.user.name.last}</td>
+        <td>{props.user.cell}</td>
+        <td>{props.user.email}</td>
+        <td>{props.user.location.timezone.description} </td>
+        </tr>
+
+    );
+  };
+  
+  export default EmployeeList;
