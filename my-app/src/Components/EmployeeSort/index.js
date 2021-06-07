ab38f0c82a48e3fa0a02cf4523eed26a63ef0b65
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-06-06 19:15:24 -0600

    switched search form to a new place and added employee sort code

---
+++ my-app/src/Components/EmployeeSort/index.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const EmployeeSort = emailSort => {
+    return (
+    <div>
+        <button onClick= {emailSort}></button>
+    </div>
+    )
+}
+
+export default EmployeeSort;
